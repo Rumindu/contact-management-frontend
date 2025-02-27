8ab6ab6e83f723ac168aa6a3dd78220c5b9ee166
Rumindu De Silva <rumindukavishka14@gmail.coma>
2025-02-27 22:55:10 +0530

    feat: add fallback route to redirect unmatched paths to the contact list

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,10 @@
 import React from "react";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Navigate,
+} from "react-router-dom";
 import ContactList from "./pages/ContactList";
 import AddContact from "./pages/AddContact";
 import EditContact from "./pages/EditContact";
@@ -11,6 +16,7 @@ const App: React.FC = () => {
         <Route path="/" element={<ContactList />} />
         <Route path="/add" element={<AddContact />} />
         <Route path="/edit/:id" element={<EditContact />} />
+        <Route path="*" element={<Navigate to="/" replace />} />
       </Routes>
     </Router>
   );
